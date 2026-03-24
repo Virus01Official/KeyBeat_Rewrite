d46cc33218adf597048fb01cf5f70f4baf7a8cf2
Virus01Official <virusidkoff@gmail.com>
2026-03-24 14:01:31 +0100

    work

--- game.gd
+++ game.gd
@@ -2,6 +2,8 @@ extends Control
 
 var down_pressed = false
 var left_pressed = false
+var up_pressed = false
+var right_pressed = false
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("down"):
@@ -12,6 +14,14 @@ func _process(_delta: float) -> void:
 		_change_visibility($Left/TextureRect, false)
 		_change_visibility($Left/Glow, true)
 		left_pressed = true
+	if Input.is_action_just_pressed("up"):
+		_change_visibility($Up/TextureRect, false)
+		_change_visibility($Up/Glow, true)
+		up_pressed = true
+	if Input.is_action_just_pressed("right"):
+		_change_visibility($Right/TextureRect, false)
+		_change_visibility($Right/Glow, true)
+		right_pressed = true
 
 	if Input.is_action_just_released("down"):
 		_change_visibility($Down/TextureRect, true)
@@ -21,6 +31,14 @@ func _process(_delta: float) -> void:
 		_change_visibility($Left/TextureRect, true)
 		_change_visibility($Left/Glow, false)
 		left_pressed = false
+	if Input.is_action_just_released("up"):
+		_change_visibility($Up/TextureRect, true)
+		_change_visibility($Up/Glow, false)
+		up_pressed = false
+	if Input.is_action_just_released("right"):
+		_change_visibility($Right/TextureRect, true)
+		_change_visibility($Right/Glow, false)
+		right_pressed = false
 		
 func _change_visibility(obj, boole) -> void:
 	obj.visible = boole
