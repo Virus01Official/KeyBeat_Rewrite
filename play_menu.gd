39fdfc35f703f3a128a1d3cf7117797c6e45936d
Virus01Official <virusidkoff@gmail.com>
2026-03-27 07:49:03 +0100

    work

--- play_menu.gd
+++ play_menu.gd
@@ -19,6 +19,8 @@ func load_songs() -> void:
 	dir.list_dir_begin()
 	var folder_name = dir.get_next()
 	
+	$Select.pressed.connect(select_song)
+	
 	while folder_name != "":
 		if dir.current_is_dir() and not folder_name.begins_with("."):
 			var song_path = maps_location + folder_name + "/song.json"
@@ -27,12 +29,15 @@ func load_songs() -> void:
 			category_container.add_child(newCategory)
 			if song_data:
 				newCategory.get_node('Category').get_node('CategoryName').text = song_data.get("title", "Unknown")
-				newCategory.get_node('Category').get_node('ScrollContainer').get_node('VBoxContainer').get_node('SongDifficulty').get_node('Button').text = song_data.get("difficulty", "Unknown")
-			
-			var button = newCategory.get_node('Category').get_node('ScrollContainer').get_node('VBoxContainer').get_node('SongDifficulty').get_node('Button')
-			button.pressed.connect(select_song.bind(
-				newCategory.get_node('Category').get_node('CategoryName').text
-			))
+				var button = newCategory.get_node('Category/ScrollContainer/VBoxContainer/SongDifficulty/Button')
+				button.text = song_data.get("difficulty", "Unknown")
+				button.pressed.connect(choose.bind(
+					song_data.get("title", "Unknown"),
+					button.text,
+					song_data.get("credits", "No one"),
+					song_data.get("mapper", "No one")
+				))
+				# image loading here too
 			
 			for ext in ["jpg", "png", "jpeg"]:
 				var image_path = maps_location + folder_name + "/background." + ext
@@ -62,6 +67,13 @@ func load_song_json(path: String) -> Dictionary:
 	
 	return json.get_data()
 	
-func select_song(song):
+func choose(song, Difficulty, credits, mapper):
+	$Selected.text = song
+	$Difficulty.text = Difficulty
+	$Credits.text = credits
+	$Mapper.text = mapper
+	
+func select_song():
+	var song = $Selected.text
 	$"..".get_node("game").visible = true
 	$"..".get_node("game")._start(song)
