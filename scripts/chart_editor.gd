c6a870f89e89e67f8a7dacea2af51d7515acb3a8
Virus01Official <virusidkoff@gmail.com>
2026-04-16 14:19:46 +0200

    working on chart editor

--- scripts/chart_editor.gd
+++ scripts/chart_editor.gd
@@ -1,5 +1,20 @@
 extends Control
 
+var note_scene = preload("res://Note.tscn")
+var chart: Array = []  
+var song_position: float = 0.0
+var song_started: bool = false
+var next_note_index: int = 0
+
+const RECEPTOR_Y = 3
+
+var offset: float = 0.0
+
+const LEAD_TIME = 2.0
+var NOTE_SPEED: float:
+	get: return GameData.Scroll_Speed
+
+@onready var note_container = $NoteContainer 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
